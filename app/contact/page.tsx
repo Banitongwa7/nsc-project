ca5a3238f94acb7f5ccfef676ed85fe85e937e68
David BANITONGWA <davidbanitongwa@gmail.com>
2025-01-19 17:23:07 +0100

    feat: add ContactPage component with greeting message

---
+++ app/contact/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ContactPage() {
+  return (
+    <div className='font-bold text-3xl text-center w-full'>Bonjour Christian Tu vas bien ?</div>
+  )
+}
