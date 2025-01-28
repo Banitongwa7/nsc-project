63732a0aa0aaac56a7ae4157697095ee84d8cfe1
David BANITONGWA <davidbanitongwa@gmail.com>
2025-01-28 11:34:58 +0100

    feat: update article images to use external URLs and configure image domains

--- data/AllArticles.ts
+++ data/AllArticles.ts
@@ -4,7 +4,7 @@ const AllArticles: ItemType[] = [
     {
         id: 1,
         name: "CeraVe 150ml",
-        image: "/assets/img/articles/cerave/image1.png",
+        image: "https://j0xsqvodkhq8q3ie.public.blob.vercel-storage.com/data/articles/cerave/image1-iL4OdKjJ5g9gm8g6KykOa7lYjUAZhL.png",
         description: "CeraVe Hydrating Cleanser was developed with dermatologists to cleanse and refresh the skin without over-stripping it or leaving it feeling tight and dry. Featuring three essential ceramides and hyaluronic acid, the gel-based foaming cleanser is an effective yet non-irritating way to cleanse and hydrate the skin.",
         marque: "CeraVe",
         price: "$20",
@@ -12,7 +12,7 @@ const AllArticles: ItemType[] = [
     {
         id: 2,
         name: "CeraVe 150ml",
-        image: "/assets/img/articles/cerave/image2.jpg",
+        image: "https://j0xsqvodkhq8q3ie.public.blob.vercel-storage.com/data/articles/cerave/image2-v8VFYsg7tiirlKoQCKBCmspWY6tg3K.jpg",
         description: "CeraVe Hydrating Cleanser was developed with dermatologists to cleanse and refresh the skin without over-stripping it or leaving it feeling tight and dry. Featuring three essential ceramides and hyaluronic acid, the gel-based foaming cleanser is an effective yet non-irritating way to cleanse and hydrate the skin.",
         marque: "CeraVe",
         price: "$25",
@@ -20,7 +20,7 @@ const AllArticles: ItemType[] = [
     {
         id: 3,
         name: "CeraVe 150ml",
-        image: "/assets/img/articles/cerave/image3.jpg",
+        image: "https://j0xsqvodkhq8q3ie.public.blob.vercel-storage.com/data/articles/cerave/image3-DUK6ghxvmN8NmL1ue1SClVxbRxVbFl.jpg",
         description: "CeraVe Hydrating Cleanser was developed with dermatologists to cleanse and refresh the skin without over-stripping it or leaving it feeling tight and dry. Featuring three essential ceramides and hyaluronic acid, the gel-based foaming cleanser is an effective yet non-irritating way to cleanse and hydrate the skin.",
         marque: "CeraVe",
         price: "$30",
@@ -28,7 +28,7 @@ const AllArticles: ItemType[] = [
     {
         id: 4,
         name: "CeraVe 150ml",
-        image: "/assets/img/articles/cerave/image4.jpg",
+        image: "https://j0xsqvodkhq8q3ie.public.blob.vercel-storage.com/data/articles/cerave/image4-SuHV4HZcvT6TCe3TMJdOrNd079EwtM.jpg",
         description: "CeraVe Hydrating Cleanser was developed with dermatologists to cleanse and refresh the skin without over-stripping it or leaving it feeling tight and dry. Featuring three essential ceramides and hyaluronic acid, the gel-based foaming cleanser is an effective yet non-irritating way to cleanse and hydrate the skin.",
         marque: "CeraVe",
         price: "$35",
@@ -36,7 +36,7 @@ const AllArticles: ItemType[] = [
     {
         id: 5,
         name: "CeraVe 150ml",
-        image: "/assets/img/articles/cerave/image5.jpg",
+        image: "https://j0xsqvodkhq8q3ie.public.blob.vercel-storage.com/data/articles/cerave/image5-Numdv91A0I00APrl8SrGOxV4KwA91H.jpg",
         description: "CeraVe Hydrating Cleanser was developed with dermatologists to cleanse and refresh the skin without over-stripping it or",
         marque: "CeraVe",
         price: "$40",
@@ -44,7 +44,7 @@ const AllArticles: ItemType[] = [
     {
         id: 6,
         name: "CeraVe 150ml",
-        image: "/assets/img/articles/cerave/image6.jpg",
+        image: "https://j0xsqvodkhq8q3ie.public.blob.vercel-storage.com/data/articles/cerave/image6-3jHlPwXxGIv6xW8cb093u62MzeacEt.jpg",
         description: "CeraVe Hydrating Cleanser was developed with dermatologists to cleanse and refresh the skin without over-stripping it or",
         marque: "CeraVe",
         price: "$45",
@@ -52,7 +52,7 @@ const AllArticles: ItemType[] = [
     {
         id: 7,
         name: "CeraVe 150ml",
-        image: "/assets/img/articles/cerave/image7.jpg",
+        image: "https://j0xsqvodkhq8q3ie.public.blob.vercel-storage.com/data/articles/cerave/image7-2b48dJMVEruAf1ScwA6xaiZZcdIChX.jpg",
         description: "CeraVe Hydrating Cleanser was developed with dermatologists to cleanse and refresh the skin without over-stripping it or",
         marque: "CeraVe",
         price: "$50",
@@ -60,7 +60,7 @@ const AllArticles: ItemType[] = [
     {
         id: 8,
         name: "CeraVe 150ml",
-        image: "/assets/img/articles/cerave/image8.jpg",
+        image: "https://j0xsqvodkhq8q3ie.public.blob.vercel-storage.com/data/articles/cerave/image8-o8JR4yV6bedTtSOqZN7EKJTlvNqqpf.jpg",
         description: "CeraVe Hydrating Cleanser was developed with dermatologists to cleanse and refresh the skin without over-stripping it or",
         marque: "CeraVe",
         price: "$55",
@@ -68,7 +68,7 @@ const AllArticles: ItemType[] = [
     {
         id: 9,
         name: "CeraVe 150ml",
-        image: "/assets/img/articles/cerave/image9.jpg",
+        image: "https://j0xsqvodkhq8q3ie.public.blob.vercel-storage.com/data/articles/cerave/image9-YKHhmfuF43qZx76fVaaTux9kb1uibM.jpg",
         description: "CeraVe Hydrating Cleanser was developed with dermatologists to cleanse and refresh the skin without over-stripping it or",
         marque: "CeraVe",
         price: "$60",
@@ -76,7 +76,7 @@ const AllArticles: ItemType[] = [
     {
         id: 10,
         name: "CeraVe 150ml",
-        image: "/assets/img/articles/cerave/image10.jpg",
+        image: "https://j0xsqvodkhq8q3ie.public.blob.vercel-storage.com/data/articles/cerave/image10-nNb6HWxYkrRjhmyypb7LAvyMvppoRk.jpg",
         description: "CeraVe Hydrating Cleanser was developed with dermatologists to cleanse and refresh the skin without over-stripping it or",
         marque: "CeraVe",
         price: "$65",
@@ -84,7 +84,7 @@ const AllArticles: ItemType[] = [
     {
         id: 11,
         name: "CeraVe 150ml",
-        image: "/assets/img/articles/cerave/image11.jpg",
+        image: "https://j0xsqvodkhq8q3ie.public.blob.vercel-storage.com/data/articles/cerave/image11-lxYWD64dEeCb6Eht4KvK0A0idj6Kr7.jpg",
         description: "CeraVe Hydrating Cleanser was developed with dermatologists to cleanse and refresh the skin without over-stripping it or",
         marque: "CeraVe",
         price: "$70",
@@ -92,7 +92,7 @@ const AllArticles: ItemType[] = [
     {
         id: 12,
         name: "CeraVe 150ml",
-        image: "/assets/img/articles/cerave/image12.jpg",
+        image: "https://j0xsqvodkhq8q3ie.public.blob.vercel-storage.com/data/articles/cerave/image12-mLtbPq9KdN93lTBtUNCC7VeYz2XggL.jpg",
         description: "CeraVe Hydrating Cleanser was developed with dermatologists to cleanse and refresh the skin without over-stripping it or",
         marque: "CeraVe",
         price: "$75",
@@ -100,7 +100,7 @@ const AllArticles: ItemType[] = [
     {
         id: 13,
         name: "CeraVe 150ml",
-        image: "/assets/img/articles/cerave/image13.jpg",
+        image: "https://j0xsqvodkhq8q3ie.public.blob.vercel-storage.com/data/articles/cerave/image13-yCsKPyHoImeRV26c2ThwzjvW8eKOOf.jpg",
         description: "CeraVe Hydrating Cleanser was developed with dermatologists to cleanse and refresh the skin without over-stripping it or",
         marque: "CeraVe",
         price: "$80",
@@ -108,7 +108,7 @@ const AllArticles: ItemType[] = [
     {
         id: 14,
         name: "CeraVe 150ml",
-        image: "/assets/img/articles/cerave/image14.jpg",
+        image: "https://j0xsqvodkhq8q3ie.public.blob.vercel-storage.com/data/articles/cerave/image14-j8fgmw4o1YoV8Fhguq6exIhHavHfa4.jpg",
         description: "CeraVe Hydrating Cleanser was developed with dermatologists to cleanse and refresh the skin without over-stripping it or",
         marque: "CeraVe",
         price: "$85",
@@ -116,7 +116,7 @@ const AllArticles: ItemType[] = [
     {
         id: 15,
         name: "CeraVe 150ml",
-        image: "/assets/img/articles/cerave/image15.jpg",
+        image: "https://j0xsqvodkhq8q3ie.public.blob.vercel-storage.com/data/articles/cerave/image15-2YmkWHOmWSejhQ7sVZ7Y3QdzActTHD.jpg",
         description: "CeraVe Hydrating Cleanser was developed with dermatologists to cleanse and refresh the skin without over-stripping it or",
         marque: "CeraVe",
         price: "$90",
@@ -124,7 +124,7 @@ const AllArticles: ItemType[] = [
     {
         id: 16,
         name: "CeraVe 150ml",
-        image: "/assets/img/articles/cerave/image16.jpg",
+        image: "https://j0xsqvodkhq8q3ie.public.blob.vercel-storage.com/data/articles/cerave/image16-ws6i0cdVQwSwFDL23kF6xyyGtsMSrJ.jpg",
         description: "CeraVe Hydrating Cleanser was developed with dermatologists to cleanse and refresh the skin without over-stripping it or",
         marque: "CeraVe",
         price: "$95",
@@ -132,7 +132,7 @@ const AllArticles: ItemType[] = [
     {
         id: 17,
         name: "CeraVe 150ml",
-        image: "/assets/img/articles/cerave/image17.jpg",
+        image: "https://j0xsqvodkhq8q3ie.public.blob.vercel-storage.com/data/articles/cerave/image17-OHylGvdsliCXnRqjWnlO0Yv8WY93fE.jpg",
         description: "CeraVe Hydrating Cleanser was developed with dermatologists to cleanse and refresh the skin without over-stripping it or",
         marque: "CeraVe",
         price: "$100",
@@ -140,7 +140,7 @@ const AllArticles: ItemType[] = [
     {
         id: 18,
         name: "CeraVe 150ml",
-        image: "/assets/img/articles/cerave/image18.jpg",
+        image: "https://j0xsqvodkhq8q3ie.public.blob.vercel-storage.com/data/articles/cerave/image18-RsN9J52pc0VriSt0QUxPUDvXSAutZG.jpg",
         description: "CeraVe Hydrating Cleanser was developed with dermatologists to cleanse and refresh the skin without over-stripping it or",
         marque: "CeraVe",
         price: "$105",
@@ -148,7 +148,7 @@ const AllArticles: ItemType[] = [
     {
         id: 19,
         name: "CeraVe 150ml",
-        image: "/assets/img/articles/cerave/image19.jpg",
+        image: "https://j0xsqvodkhq8q3ie.public.blob.vercel-storage.com/data/articles/cerave/image19-0lVok3FLHILGbMinPuaRfkjGTlQ1jc.jpg",
         description: "CeraVe Hydrating Cleanser was developed with dermatologists to cleanse and refresh the skin without over-stripping it or",
         marque: "CeraVe",
         price: "$110",
@@ -156,7 +156,7 @@ const AllArticles: ItemType[] = [
     {
         id: 20,
         name: "CeraVe 150ml",
-        image: "/assets/img/articles/cerave/image20.jpg",
+        image: "https://j0xsqvodkhq8q3ie.public.blob.vercel-storage.com/data/articles/cerave/image20-rDGYWQCNwrGjqqt7AbwutCjspBUen9.jpg",
         description: "CeraVe Hydrating Cleanser was developed with dermatologists to cleanse and refresh the skin without over-stripping it or",
         marque: "CeraVe",
         price: "$115",
@@ -164,7 +164,7 @@ const AllArticles: ItemType[] = [
     {
         id: 21,
         name: "CeraVe 150ml",
-        image: "/assets/img/articles/cerave/image21.jpg",
+        image: "https://j0xsqvodkhq8q3ie.public.blob.vercel-storage.com/data/articles/cerave/image21-VCmpaArhoicSqpa4z58OH62MnpF7Lo.jpg",
         description: "CeraVe Hydrating Cleanser was developed with dermatologists to cleanse and refresh the skin without over-stripping it or",
         marque: "CeraVe",
         price: "$120",
@@ -172,7 +172,7 @@ const AllArticles: ItemType[] = [
     {
         id: 22,
         name: "CeraVe 150ml",
-        image: "/assets/img/articles/cerave/image22.jpg",
+        image: "https://j0xsqvodkhq8q3ie.public.blob.vercel-storage.com/data/articles/cerave/image22-Om9c8ZjpRRAq2y7HVxUXRMuWJxedQa.jpg",
         description: "CeraVe Hydrating Cleanser was developed with dermatologists to cleanse and refresh the skin without over-stripping it or",
         marque: "CeraVe",
         price: "$125",
@@ -180,7 +180,7 @@ const AllArticles: ItemType[] = [
     {
         id: 23,
         name: "CeraVe 150ml",
-        image: "/assets/img/articles/cerave/image23.jpg",
+        image: "https://j0xsqvodkhq8q3ie.public.blob.vercel-storage.com/data/articles/cerave/image23-xlqBuUrEVtjqQZ8gMd339Ri8UwkMRq.jpg",
         description: "CeraVe Hydrating Cleanser was developed with dermatologists to cleanse and refresh the skin without over-stripping it or",
         marque: "CeraVe",
         price: "$130",
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    images: {
+        domains: ["j0xsqvodkhq8q3ie.public.blob.vercel-storage.com"],
+    },
 };
 
 export default nextConfig;
